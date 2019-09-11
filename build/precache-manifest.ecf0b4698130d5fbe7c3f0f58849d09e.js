69d7978eaf0fc185a93bf943d491567cfafb6128
jiaxinlin0 <jiaxin950425@icloud.com>
2019-09-11 12:18:29 -0400

    add html and css accordingly

---
+++ build/precache-manifest.ecf0b4698130d5fbe7c3f0f58849d09e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e67e492d89fbdeee8b5969e4baab88b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4f4aa9e86606d9efcb8",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6fb942ce2f925a18dd93",
+    "url": "/static/js/2.25a60e3e.chunk.js"
+  },
+  {
+    "revision": "a4f4aa9e86606d9efcb8",
+    "url": "/static/js/main.4be3a805.chunk.js"
+  },
+  {
+    "revision": "b7c6d5bbf0e69e52b67d",
+    "url": "/static/js/runtime~main.13b3e6e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
